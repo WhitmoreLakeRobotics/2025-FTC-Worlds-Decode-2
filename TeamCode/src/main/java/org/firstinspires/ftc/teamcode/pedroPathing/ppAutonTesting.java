0910eaac3f93a66079e59d36c705e5500c33b8fe
Titanium Trojans <32377135+Jewellnat@users.noreply.github.com>
2026-02-26 16:17:08 -0500

    naj updated ppAutonTesting
    This was part of initial setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ppAutonTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ppAutonTesting.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import static org.firstinspires.ftc.teamcode.pedroPathing.CompBotConstants.pathConstraints;
+
 import com.bylazar.configurables.PanelsConfigurables;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
@@ -9,12 +13,15 @@ import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-@Autonomous(name = "ppTesting", group = "PP")
+@Autonomous(name = "ppAutonTesting", group = "PP")
 public class ppAutonTesting extends OpMode{
 
+
+    private String thisUpdate = "0";
+    private TelemetryManager telemetryMU;
+
+
     public static Follower follower;
-
-
     public static Pose startPose = new Pose(57, 9, Math.toRadians(90)); // Start Pose of our robot.
     public static Pose scorePose = new Pose(57, 15, Math.toRadians(114)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     //private final Pose scorePose = new Pose(wallScoreX, wallScoreY, wallScoreH); // seeing if configurables work for this. Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
@@ -45,18 +52,28 @@ public class ppAutonTesting extends OpMode{
     @Override
     public void init() {
 
+// NAJ CompBotConstants is a file/class that contains the definition of the gryo and drive motors among other things
 
         follower =  CompBotConstants.createFollower(hardwareMap);
         buildPaths();
         PanelsConfigurables.INSTANCE.refreshClass(this);
         follower.setStartingPose(startPose);
         follower.update();
+//  pedroPanelsTelemetry.init();
+        Drawing.init();
+        telemetryMU = PanelsTelemetry.INSTANCE.getTelemetry();
 
-
-
-
+// disp[lay starting postition
+        telemetryMU.addData("initialized postition - Update ", thisUpdate);
+// Feedback to Driver Hub for debugging
+        updateTelemetry();
 
     }
+
+
+
+
+
 
     @Override
     public void init_loop() {
@@ -77,4 +94,30 @@ public class ppAutonTesting extends OpMode{
     public void stop() {
         super.stop();
     }
+
+private void updateTelemetry() {
+    telemetryMU.addData("Follower Busy?", follower.isBusy());
+   // telemetryMU.addData("Current Stage", currentStage);
+    telemetryMU.addData("x", follower.getPose().getX());
+    telemetryMU.addData("y", follower.getPose().getY());
+    telemetryMU.addData("heading", Math.toDegrees(follower.getPose().getHeading()));
+  //  telemetryMU.addData("LAST Pose", lastPose);
+  //  telemetryMU.addData("Current Target Pose", currentTargetPose);
+    telemetryMU.addData("breakingStrength", pathConstraints.getBrakingStrength());
+    telemetryMU.addData("breakstart ", pathConstraints.getBrakingStart());
+    telemetryMU.addData("drivepid P", follower.constants.coefficientsDrivePIDF.P );
+    telemetryMU.addData("drivepid D", follower.constants.coefficientsDrivePIDF.D );
+    telemetryMU.addData("drivepid F", follower.constants.coefficientsDrivePIDF.F );
+    telemetryMU.addData("CONSTRAINTS", "");
+    telemetryMU.addData("Tvalue (% complete)", follower.pathConstraints.getTValueConstraint());
+    telemetryMU.addData("Current tValue", follower.getCurrentTValue());
+    telemetryMU.addData("Velocity Constraint", follower.pathConstraints.getVelocityConstraint());
+    telemetryMU.addData("Current Velocity", follower.getVelocity());
+    telemetryMU.addData("Trans constraint", follower.pathConstraints.getTranslationalConstraint());
+    // telemetryMU.addData("current Trans", follower.getTranslationalError());
+    telemetryMU.addData("Heading Constraint", follower.pathConstraints.getHeadingConstraint());
+
+    telemetryMU.update();
+    Drawing.drawDebug(follower);
+}
 }
